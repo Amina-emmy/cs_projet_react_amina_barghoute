bc45c3bcdc6f3ceca4a59ac656ce2f4e82bf2f27
Amina-emmy <amina.barghoute2001@gmail.com>
2023-07-27 14:49:25 +0100

    page product lg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Error } from "./Error";
 import { Home } from "./pages/home/Home";
@@ -6,21 +6,67 @@ import { About } from "./pages/about/About";
 import { Product } from "./pages/Product/Product";
 import { Contact } from "./pages/Contact/Contact";
 import { Header } from "./layouts/Header";
-import {Heart} from "./pages/Favorite/Heart";
+import { Heart } from "./pages/Favorite/Heart";
 import { Panier } from "./pages/Panier/Panier";
+//^ Women => products images
+import wm1 from "./assets/images/products/women/women1.jpg";
+import wm2 from "./assets/images/products/women/women2.jpg";
+import wm3 from "./assets/images/products/women/women3.jpg";
+import wm4 from "./assets/images/products/women/women4.jpg";
+//^ Men => products images
+import m1 from "./assets/images/products/men/men1.jpg";
+import m2 from "./assets/images/products/men/men2.jpg";
+import m3 from "./assets/images/products/men/men3.jpg";
+import m4 from "./assets/images/products/men/men4.jpg";
+//^ Kids => products images
+import k1 from "./assets/images/products/kids/kid1.jpg";
+import k2 from "./assets/images/products/kids/kid2.jpg";
+import k3 from "./assets/images/products/kids/kid3.jpg";
+import k4 from "./assets/images/products/kids/kid4.jpg";
+// Accessories => products images
 
 export const App = () => {
+  //^ for shopping
+  const product =(src, name, price, wanted, category, tag) =>({
+    src,
+    name,
+    price,
+    wanted,
+    category,
+    tag
+  });
+  const [products,setProducts]=useState([
+    product(wm1, "T-shirt purple", 120, 1, "women", "new"),
+    product(wm2, "brown pants", 150, 1, "women", "sale"),
+    product(wm3, "Chemise blue", 220, 1, "women", "best seller"),
+    product(wm4, "outfit vintage", 70, 1, "women", "old"),
+
+    product(m1, "chemise", 120, 1, "men", "new"),
+    product(m2, "chemise noir", 50, 1, "men", "sale"),
+    product(m3, "jacket brown", 200, 1, "men", "best seller"),
+    product(m4, "jacket cool", 70, 1, "men", "old"),
+
+    product(k1,"skirt&coat pink", 120, 1, "kids", "new"),
+    product(k2, "skirt&coat green", 50, 1, "kids", "sale"),
+    product(k3, "outfit brown", 200, 1, "kids", "best seller"),
+    product(k4, "outfit green", 70, 1, "kids", "old")
+  ]);
+  
+  //* tables shopping
+  // const [panier,setPanier]=useState([]);
+  // const [fav,setFav]=useState([]);
+
   return (
     <>
       <Header />
       <Routes>
         <Route path="*" element={<Error />} />
         <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/product" element={<Product products={products} setProducts={setProducts}/>} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
 
-        <Route path="/heart" element={<Heart/>} />
+        <Route path="/heart" element={<Heart />} />
         <Route path="/panier" element={<Panier />} />
       </Routes>
     </>
--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,14 +1,155 @@
 import "./_Product.scss"
 import { Footer } from "../../layouts/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { useState } from "react";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-export const Product=()=>{
+export const Product = (props) => {
+    const [all, setAll] = useState(false);
+    const [women, setWomen] = useState(false);
+    const [men, setMen] = useState(false);
+    const [kids, setKids] = useState(false);
 
-    return(<>
+    const [search, setSearch] = useState("");
+    const [filterElement, setFilterElement] = useState([]);
+
+    const searchProduct = (event) => {
+        setWomen(false);
+        setAll(false);
+        setMen(false);
+        setKids(false);
+        let inpV = event.target.value;
+        setSearch(inpV);
+        const newProducts = [...props.products];
+        const searchedProduct = newProducts.filter((element) => element.name.includes(search));
+        setFilterElement(searchedProduct);
+    }
+
+    return (<>
         <section>
             <div className="bgpic">
                 <div className="bgtitre text-light"><h1>PRODUCT</h1></div>
             </div>
+            <div className="d-flex gap-6 justify-content-evenly m-7">
+                <div className="ms-8 pe-5 w-25">
+                    <div>
+                        <h4>Categories</h4>
+                        <ul className="navbar-nav">
+                            <li className="li_category" onClick={() => {
+                                setAll(true);
+                                setWomen(false);
+                                setMen(false);
+                                setKids(false);
+                            }}>All</li>
+                            <li className="li_category" onClick={() => {
+                                setWomen(true);
+                                setAll(false);
+                                setMen(false);
+                                setKids(false);
+                            }}>Women</li>
+                            <li className="li_category" onClick={() => {
+                                setMen(true);
+                                setWomen(false);
+                                setAll(false);
+                                setKids(false);
+                            }}>Men</li>
+                            <li className="li_category" onClick={() => {
+                                setKids(true);
+                                setWomen(false);
+                                setAll(false);
+                                setMen(false);
+                            }}>Kids</li>
+                        </ul>
+                    </div>
+                    <div className="mt-3">
+                        <h4>Filter</h4>
+                        <input type="text" placeholder="Search Products.." name="search" onChange={(event) => {
+                            searchProduct(event);
+                        }} />
+                        <FontAwesomeIcon className="ms-1 opacity-75" icon={faSearch} />
+                    </div>
+                </div>
 
+                <div className="d-flex flex-wrap gap-2 divallProducts">
+                    {
+                        (all === true && search ==="") || (all === false && women === false && kids === false && men === false && search ==="") ?
+                            <>
+                                {props.products.map((element) => <>
+                                    <div className="picProduct">
+                                        <img src={element.src} alt="" width="200px" />
+                                        <h4>{element.name}</h4>
+                                        <p>{element.price} MAD</p>
+                                        <button className="btnCart btn btn-dark rounded-pill px-3">ADD TO CART</button>
+                                        <button className="btnSale btn btn-orange text-light rounded-pill">{element.tag}</button>
+                                        <button className="btnFav btn border-0"><FontAwesomeIcon icon={faHeart} className="fs-5" /></button>
+                                    </div>
+                                </>)}
+                            </>
+                            : women === true && search ==="" ?
+                                <>
+                                    {props.products.map((element) =>
+                                        element.category === "women" ?
+                                            <>
+                                                <div className="picProduct">
+                                                    <img src={element.src} alt="" width="200px" />
+                                                    <h4>{element.name}</h4>
+                                                    <p>{element.price} MAD</p>
+                                                    <button className="btnCart btn btn-dark rounded-pill px-3">ADD TO CART</button>
+                                                    <button className="btnSale btn btn-orange text-light rounded-pill">{element.tag}</button>
+                                                    <button className="btnFav btn border-0"><FontAwesomeIcon icon={faHeart} className="fs-5" /></button>
+                                                </div>
+                                            </> : <></>)}
+                                </>
+                                : men === true && search ==="" ?
+                                    <>
+                                        {props.products.map((element) =>
+                                            element.category === "men" ?
+                                                <>
+                                                    <div className="picProduct">
+                                                        <img src={element.src} alt="" width="200px" />
+                                                        <h4>{element.name}</h4>
+                                                        <p>{element.price} MAD</p>
+                                                        <button className="btnCart btn btn-dark rounded-pill px-3">ADD TO CART</button>
+                                                        <button className="btnSale btn btn-orange text-light rounded-pill">{element.tag}</button>
+                                                        <button className="btnFav btn border-0"><FontAwesomeIcon icon={faHeart} className="fs-5" /></button>
+                                                    </div>
+                                                </> : <></>)}
+                                    </>
+                                    : kids === true && search ==="" ?
+                                        <>
+                                            {props.products.map((element) =>
+                                                element.category === "kids" ?
+                                                    <>
+                                                        <div className="picProduct">
+                                                            <img src={element.src} alt="" width="200px" />
+                                                            <h4>{element.name}</h4>
+                                                            <p>{element.price} MAD</p>
+                                                            <button className="btnCart btn btn-dark rounded-pill px-3">ADD TO CART</button>
+                                                            <button className="btnSale btn btn-orange text-light rounded-pill">{element.tag}</button>
+                                                            <button className="btnFav btn border-0"><FontAwesomeIcon icon={faHeart} className="fs-5" /></button>
+                                                        </div>
+                                                    </> : <></>)}
+                                        </>
+                                        :
+                                        <>
+                                        </>
+                    }
+                    <div className="d-flex flex-wrap gap-2 w-50">
+                        {filterElement.map((element) =>
+                            <>
+                                <div className="picProduct">
+                                    <img src={element.src} alt="" width="200px" />
+                                    <h4>{element.name}</h4>
+                                    <p>{element.price} MAD</p>
+                                    <button className="btnCart btn btn-dark rounded-pill px-3">ADD TO CART</button>
+                                    <button className="btnSale btn btn-orange text-light rounded-pill">{element.tag}</button>
+                                    <button className="btnFav btn border-0"><FontAwesomeIcon icon={faHeart} className="fs-5" /></button>
+                                </div>
+                            </>)}
+                    </div>
+                </div>
+            </div>
             <div>
                 <Footer />
             </div>
